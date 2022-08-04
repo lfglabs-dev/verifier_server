de6ab7e1e9c1a1e5d541768889110173444f820b
Thomas Marchand <thomas.marchand@tuta.io>
2022-08-04 10:49:32 +0200

    return strings

--- verifier/server/http.py
+++ verifier/server/http.py
@@ -35,8 +35,8 @@ class WebServer:
                         "user_id": user_id,
                         "username": username,
                         "discriminator": discriminator,
-                        "sign0": sign0,
-                        "sign1": sign1,
+                        "sign0": str(sign0),
+                        "sign1": str(sign1),
                     }
                 )
             except Exception:
@@ -61,8 +61,8 @@ class WebServer:
                         "user_id": user_id,
                         "username": username,
                         "name": name,
-                        "sign0": sign0,
-                        "sign1": sign1,
+                        "sign0": str(sign0),
+                        "sign1": str(sign1),
                     }
                 )
             except Exception:
